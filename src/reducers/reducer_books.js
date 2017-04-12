9fa9437aafa4c34b09b24870a12ccbcfb689db38
Kamilla <venessiel@gmail.com>
2017-04-12 08:44:12 -0400

    add books container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+//reducers generate parts of the application state
+
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts' },
+		{ title: 'Harry Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' }
+	];
+};
+
